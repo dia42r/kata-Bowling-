26a9afc7be707fe5a0ec6a43da0979964022408d
Abdoul DIARRA <abdoul.diarra-prestataire@laposte.fr>
2019-06-03 17:42:25 +0200

    Score calculation for perfect game, spares frames games, open frame games and  mixed frame game

--- src/main/java/org/codingdojo/bowling/Game.java
+++ src/main/java/org/codingdojo/bowling/Game.java
@@ -4,23 +4,52 @@ import java.util.List;
 
 public class Game {
 
+    private static final Integer NB_MAX_PIN = 10;
+    private static final Integer NB_FRAME = 10;
+
     private Integer score = 0;
-    private Integer nbFrame = 1;
     private Integer bonusBall = 0;
 
     public Integer getScore(List<Frame> frames) {
 
-        for (Frame frame: frames) {
-            if (frame.isStrike() && nbFrame < 10){
-                Integer indexOfNextFrame = frames.indexOf(frame) + 1;
-                Frame nextFrame = frames.get(indexOfNextFrame);
-                this.score += 10 + nextFrame.getFirstThrow() + nextFrame.getSecondThrow();
-            } else if (frame.isStrike() && nbFrame >= 10 ) {
-                this.bonusBall += 10;
+        Integer currentFrame = 1;
+        Integer indexOfFrame = 0;
+        for (Frame frame : frames) {
+
+            if (frame.isStrike() && currentFrame < NB_FRAME) {
+
+                Frame nextFrame = frames.get(indexOfFrame + 1);
+
+                if (nextFrame.isStrike() && (indexOfFrame + 2) < frames.size() - 1) {
+                    this.score += NB_MAX_PIN + NB_MAX_PIN + frames.get(indexOfFrame + 2).getFirstThrow();
+                } else {
+                    this.score += NB_MAX_PIN + nextFrame.getFirstThrow() + nextFrame.getSecondThrow();
+                }
+
+            } else if (frame.isStrike() && currentFrame.equals(NB_FRAME)) {
+                this.bonusBall = frame.getBonusBall().stream().reduce(0, (a, b) -> (a + b));
+                this.score += NB_MAX_PIN + this.bonusBall;
+
+            } else if (frame.isSpare() && currentFrame < NB_FRAME) {
+
+                Frame nextFrame = frames.get(indexOfFrame + 1);
+                this.score += NB_MAX_PIN + nextFrame.getFirstThrow();
+
+            } else if (frame.isSpare() && currentFrame.equals(NB_FRAME)) {
+
+                this.bonusBall = frame.getBonusBall().stream().reduce(0, (a, b) -> (a + b));
+                this.score += NB_MAX_PIN + this.bonusBall;
+
+            } else if (frame.isOpen()){
+                this.score += frame.getFirstThrow() + frame.getSecondThrow();
+            } else {
+                throw new IllegalArgumentException("Invalid Frame : Throw must be between [0 - 10]") ;
             }
-        this.nbFrame++;
+
+            currentFrame++;
+            indexOfFrame++;
         }
 
-        return this.score + this.bonusBall;
+        return this.score;
     }
 }
--- src/main/java/org/codingdojo/bowling/MainApp.java
+++ src/main/java/org/codingdojo/bowling/MainApp.java
@@ -1,5 +1,4 @@
 package org.codingdojo.bowling;
-
 public class MainApp {
 
     public static void main(String[] args) {
