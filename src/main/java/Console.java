cca2debeecffeb649dc5c6a7297f50461148bc8a
Abdoul DIARRA <abdoul.diarra-prestataire@laposte.fr>
2019-06-04 06:54:18 +0200

    Refactor class Console : - Split function run - use Slf4j log instead println

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -2,9 +2,13 @@ import org.codingdojo.bowling.Frame;
 import org.codingdojo.bowling.Game;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Console {
 
     public void run() {
@@ -14,57 +18,82 @@ public class Console {
 
         for (int i = 1; i <= nbFrame; i++) {
 
-            System.out.println(" ** Frame " + i + " ********************************************************************************* ");
-            System.out.println(" **** Try 1 : Number between [0 - 10] for number of pin down ** ");
             Scanner scanner = new Scanner(System.in);
-            Integer aTry_1 = scanner.nextInt();
-            System.out.println(" == " + aTry_1);
+            Integer aTry_1 = this.readEntry(i,1,  scanner);
 
-            if (aTry_1 == 10) {
-                System.out.println(" ** Strike ! **");
+            log.info(" == " + aTry_1);
+
+            if (aTry_1.equals(10)) {
+                log.info(" ** Strike ! **");
                 Frame frame = new Frame(10,10);
                 frames.add(frame);
 
                 if (i == 10) {
-                    System.out.println(" **** You have 2 Bonus Ball : Number between [0 - 10] for number of pin down ** ");
-                    System.out.println(" **** Bonus 1 Try  : Number between [0 - 10] for number of pin down ** ");
-                    Integer bonus1 = scanner.nextInt();
-
-                    System.out.println(" **** Bonus 2 Try  : Number between [0 - 10] for number of pin down ** ");
-                    Integer bonus2 = scanner.nextInt();
-
-                    frame.getBonusBall().add(bonus1);
-                    frame.getBonusBall().add(bonus2);
+                    spikeBonus(scanner, frame);
                 }
 
                 continue;
             }
 
-            System.out.println(" **** Try 2 : Number between [0 - 10] for number of pin down ** ");
-            Integer aTry_2 = scanner.nextInt();
+            Integer aTry_2 = this.readEntry(i, 2,scanner);
 
             Frame frame = new Frame(aTry_1, aTry_2);
 
-            if (Integer.valueOf(aTry_1) + Integer.valueOf(aTry_2) == 10) {
-                System.out.println(" ** Spare ! **");
+            if (aTry_1 + aTry_2 == 10) {
+                log.info(" ** Spare ! **");
 
                 if (i == 10) {
-                    System.out.println(" ****** You have 1 Bonus Ball : Number between [0 - 10] for number of pin down ** ");
-                    System.out.println(" ****** Bonus 1 Try  : Number between [0 - 10] for number of pin down ** ");
-
-                    Integer bonus1 = scanner.nextInt();
-                    frame.getBonusBall().add(bonus1);
+                    spareBonus(scanner, frame);
                 }
             }
 
             frames.add(frame);
         }
 
-        frames.forEach(frame -> {
-            System.out.println("[" +frame.getFirstThrow() + ", " + frame.getSecondThrow() + "]" );
-        });
+        frames.forEach(frame -> log.info("[" +frame.getFirstThrow() + ", " + frame.getSecondThrow() + "]" ));
 
-        System.out.println(" Your Score : " + game.getScore(frames));
+        log.info(" Your Score : " + game.getScore(frames));
 
+    }
+
+    private Integer readEntry(Integer frameNumber, Integer tryNumber, Scanner scanner) {
+        Boolean valide = false;
+        Integer entry = null;
+        while (!valide) {
+            log.info(" ** Frame " + frameNumber + " ********************************************************************************* ");
+            log.info(" **** Try " + tryNumber + " : Number between [0 - 10] for number of pin down ** ");
+            try {
+                entry = scanner.nextInt();
+                valide = true;
+            } catch (IllegalArgumentException e) {
+                log.info("Error : Number must be between [0 - 10]");
+                valide = false;
+            } catch (InputMismatchException exception) {
+                log.info("Error : Number must be Integer [0 - 10]");
+                valide = false;
+            }
+        }
+        return entry;
+    }
+
+    private void spareBonus(Scanner scanner, Frame frame) {
+        log.info(" ****** You have 1 Bonus Ball : Number between [0 - 10] for number of pin down ** ");
+        log.info(" ****** Bonus 1 Try  : Number between [0 - 10] for number of pin down ** ");
+
+        Integer bonus1 = scanner.nextInt();
+        frame.getBonusBall().add(bonus1);
+    }
+
+    private void spikeBonus(Scanner scanner, Frame frame) {
+
+        log.info(" **** You have 2 Bonus Ball : Number between [0 - 10] for number of pin down ** ");
+        log.info(" **** Bonus 1 Try  : Number between [0 - 10] for number of pin down ** ");
+        Integer bonus1 = scanner.nextInt();
+
+        log.info(" **** Bonus 2 Try  : Number between [0 - 10] for number of pin down ** ");
+        Integer bonus2 = scanner.nextInt();
+
+        frame.getBonusBall().add(bonus1);
+        frame.getBonusBall().add(bonus2);
     }
 }
--- src/main/java/org/codingdojo/bowling/Console.java
+++ src/main/java/org/codingdojo/bowling/Console.java
@@ -1,9 +1,13 @@
 package org.codingdojo.bowling;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+@Slf4j
 public class Console implements Customer {
 
     @Override
@@ -12,57 +16,92 @@ public class Console implements Customer {
         List<Frame> frames = new ArrayList<>();
 
         for (int i = 1; i <= Game.NB_FRAME; i++) {
+            log.info(" ** Frame " + i + " ********************************************************************************* ");
 
-            System.out.println(" ** Frame " + i + " ********************************************************************************* ");
-            System.out.println(" **** Try 1 : Number between [0 - 10] for number of pin down ** ");
             Scanner scanner = new Scanner(System.in);
-            Integer aTry_1 = scanner.nextInt();
-            System.out.println(" == " + aTry_1);
 
-            if (aTry_1 == Game.NB_MAX_PIN) {
-                System.out.println(" ** Strike ! **");
+            Integer aTry_1 = this.readEntry(i,1,scanner);
+
+            log.info(" == " + aTry_1);
+
+            if (aTry_1.equals(Game.NB_MAX_PIN)) {
+                log.info(" ** Strike ! **");
                 Frame frame = new Frame(10,10);
                 frames.add(frame);
 
                 if (i == Game.NB_FRAME) {
-                    System.out.println(" **** You have 2 Bonus Ball : Number between [0 - 10] for number of pin down ** ");
-                    System.out.println(" **** Bonus 1 Try  : Number between [0 - 10] for number of pin down ** ");
-                    Integer bonus1 = scanner.nextInt();
-
-                    System.out.println(" **** Bonus 2 Try  : Number between [0 - 10] for number of pin down ** ");
-                    Integer bonus2 = scanner.nextInt();
-
-                    frame.getBonusBall().add(bonus1);
-                    frame.getBonusBall().add(bonus2);
+                    this.spikeBonus(scanner, frame);
                 }
 
                 continue;
             }
 
-            System.out.println(" **** Try 2 : Number between [0 - 10] for number of pin down ** ");
-            Integer aTry_2 = scanner.nextInt();
+            Integer aTry_2 = this.readEntry(i,2,scanner);
 
             Frame frame = new Frame(aTry_1, aTry_2);
 
-            if (Integer.valueOf(aTry_1) + Integer.valueOf(aTry_2) == Game.NB_MAX_PIN) {
-                System.out.println(" ** Spare ! **");
+            if (aTry_1 + aTry_2 == Game.NB_MAX_PIN) {
+                log.info(" ** Spare ! **");
 
                 if (i == Game.NB_FRAME) {
-                    System.out.println(" ****** You have 1 Bonus Ball : Number between [0 - 10] for number of pin down ** ");
-                    System.out.println(" ****** Bonus 1 Try  : Number between [0 - 10] for number of pin down ** ");
-
-                    Integer bonus1 = scanner.nextInt();
-                    frame.getBonusBall().add(bonus1);
+                    this.spareBonus(scanner, frame);
                 }
             }
 
             frames.add(frame);
         }
 
-        frames.forEach(frame -> {
-            System.out.println("[" +frame.getFirstThrow() + ", " + frame.getSecondThrow() + "]" );
-        });
+        frames.forEach(frame -> log.info("[" +frame.getFirstThrow() + ", " + frame.getSecondThrow() + "]" ));
 
-        System.out.println(" Your Score : " + game.getScore(frames));
+        log.info(" Your Score : " + game.getScore(frames));
     }
+
+    private Integer readEntry(Integer frameNumber, Integer tryNumber , Scanner scanner) {
+
+        log.info(" **** Try " + tryNumber +" : Number between [0 - 10] for number of pin down ** ");
+
+        boolean valide = false;
+        Integer entry = null;
+        while(!valide) {
+            try {
+                entry = scanner.nextInt();
+                valide = isValideEntry(entry);
+
+                if (!valide) {
+
+                    log.error("Number must be between [0 - 10]");
+                }
+            } catch (InputMismatchException e) {
+                log.error(" Entry must be Integer :  ");
+                scanner.next();
+            }
+        }
+        return entry;
+    }
+
+
+    private boolean isValideEntry(Integer entry) {
+        return entry <= 10;
+    }
+
+    private void spikeBonus(Scanner scanner, Frame frame) {
+        log.info(" **** You have 2 Bonus Ball : Number between [0 - 10] for number of pin down ** ");
+        log.info(" **** Bonus 1 Try  : Number between [0 - 10] for number of pin down ** ");
+        Integer bonus1 = scanner.nextInt();
+
+        log.info(" **** Bonus 2 Try  : Number between [0 - 10] for number of pin down ** ");
+        Integer bonus2 = scanner.nextInt();
+
+        frame.getBonusBall().add(bonus1);
+        frame.getBonusBall().add(bonus2);
+    }
+
+    private void spareBonus(Scanner scanner, Frame frame) {
+        log.info(" ****** You have 1 Bonus Ball : Number between [0 - 10] for number of pin down ** ");
+        log.info(" ****** Bonus 1 Try  : Number between [0 - 10] for number of pin down ** ");
+
+        Integer bonus1 = scanner.nextInt();
+        frame.getBonusBall().add(bonus1);
+    }
+
 }
